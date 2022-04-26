f10f2e45e8505a96d94e59a4c36aa6ca26a1d75d
Lenildo Lourenço <lourencolenildo@gmail.com>
2022-04-26 18:54:32 -0300

    Criado estrutura base dos projetos e implementação das regras de negócio.

---
+++ src/FastTech.Domain/Entidades/Produto.cs
@@ -0,0 +1,68 @@
+﻿using FastTech.Domain.Enums;
+
+namespace FastTech.Domain.Entidades;
+internal class Produto : Entity
+{
+    public Produto(string nome, string descricao, decimal valor, 
+        TipoProduto tipo, int quantidadeEstoque)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Ativo = true;
+        Valor = valor;
+        Cadastro = DateTime.UtcNow;
+        Tipo = tipo;
+        QuantidadeEstoque = quantidadeEstoque;
+    }
+
+    public string Nome { get; private set; }
+    public string Descricao { get; private set; }
+    public bool Ativo { get; private set; }
+    public decimal Valor { get; private set; }
+    public DateTime Cadastro { get; private set; }
+    public TipoProduto Tipo { get; private set; }
+    public int QuantidadeEstoque { get; private set; }
+    
+    public void Ativar() => Ativo = true;
+
+    public void Desativar() => Ativo = false;
+
+    public void AlterarTipo(TipoProduto novotipo) => Tipo = novotipo;
+
+    public void AlterarNome(string novoNome)
+    {
+        if (string.IsNullOrWhiteSpace(novoNome))
+            throw new Exception("Nome Inválido.");
+
+        Nome = novoNome;
+    }
+
+    public void AlterarDescricao(string novaDescricao)
+    {
+        if (string.IsNullOrWhiteSpace(novaDescricao))
+            throw new Exception("Descrição Inválida.");
+
+        Descricao = novaDescricao;
+    }
+
+    public void DebitarEstoque(int quantidade)
+    {
+        if (quantidade < 0)
+            throw new Exception("Quantidade Inválida.");
+
+        if (!PossuiEstoque(quantidade))
+        {
+            throw new Exception("Quantidade insuficiente no Estoque.");
+        }
+
+        QuantidadeEstoque -= quantidade;
+    }
+
+    public bool PossuiEstoque(int quantidade) => QuantidadeEstoque >= quantidade;
+
+    public void AdicionarEstoque(int quantidade)
+    {
+        QuantidadeEstoque += quantidade;
+    }
+}
+
